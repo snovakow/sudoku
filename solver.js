81857d91d7d66917e8c60b3ed1a044e683cadd22
ScottNovakowski <snovakow@gmail.com>
2024-05-28 11:08:55 -0400

    Update solver.js

--- solver.js
+++ solver.js
@@ -1,76 +1,14 @@
-class Hit {
-	constructor(row, col, symbol) {
-		this.row = row;
-		this.col = col;
-		this.symbol = col;
-	}
-}
-
-const fillBruteForce = (grid) => {
-	const hits = [];
-
-	const foundSymbolsRow = [false, false, false, false, false, false, false, false, false];
-	const foundSymbolsCol = [false, false, false, false, false, false, false, false, false];
-	const foundSymbolsBox = [false, false, false, false, false, false, false, false, false];
-	for (let i = 0; i < 9; i++) {
-		foundSymbolsRow.fill(false);
-		foundSymbolsCol.fill(false);
-		foundSymbolsBox.fill(false);
-
-		for (let j = 0; j < 9; j++) {
-			const symbolRow = grid.getSymbol(i, j);
-			const symbolCol = grid.getSymbol(j, i);
-			const symbolBox = grid.getSymbol(Math.floor(i / 3), j % 3);
-			if (symbolRow !== 0) foundSymbolsRow[symbol - 1] = true;
-			if (symbolCol !== 0) foundSymbolsCol[symbol - 1] = true;
-			if (symbolBox !== 0) foundSymbolsBox[symbol - 1] = true;
-		}
-
-		let foundIndexRow = -1;
-		let foundIndexCol = -1;
-		let foundIndexBox = -1;
-		for (let j = 0; j < 9; j++) {
-			const foundRow = foundSymbolsRow[j];
-			const foundCol = foundSymbolsCol[j];
-			const foundBox = foundSymbolsBox[j];
-
-			if (!foundRow) {
-				if (foundIndexRow === -1) {
-					foundIndexRow = j;
-				} else {
-					foundIndexRow = -2;
-				}
-			}
-			if (!foundCol) {
-				if (foundIndexCol === -1) {
-					foundIndexCol = j;
-				} else {
-					foundIndexCol = -2;
-				}
-			}
-			if (!foundBox) {
-				if (foundIndexBox === -1) {
-					foundIndexBox = j;
-				} else {
-					foundIndexBox = -2;
-				}
-			}
-		}
-
-		if (foundIndexRow >= 0) hits.push(new Hit(i, foundIndexRow, j + 1));
-		if (foundIndexCol >= 0) hits.push(new Hit(foundIndexCol, i, j + 1));
-		if (foundIndexBox >= 0) hits.push(new Hit(Math.floor(i / 3), foundIndexBox % 3, j + 1));
-	}
-
-	return hits;
-}
-
-const fillMarkers = (grid, markers, max = 4) => {
-	markers.length = 0;
-
+const fillMarkers = (grid, markers) => {
 	for (let i = 0; i < 81; i++) {
 		if (grid[i] === 0) {
-			markers[i] = [true, true, true, true, true, true, true, true, true];
+			const marker = markers[i];
+			if (marker) {
+				for (let i = 0; i < 9; i++) {
+					if (marker[i] !== false) marker[i] = true;
+				}
+			} else {
+				markers[i] = [true, true, true, true, true, true, true, true, true];
+			}
 		}
 	}
 	for (let r = 0; r < 9; r++) {
@@ -79,7 +17,7 @@ const fillMarkers = (grid, markers, max = 4) => {
 			if (symbol > 0) continue;
 
 			const index = r * 9 + c;
-			let marker = markers[index];
+			const marker = markers[index];
 			if (!marker) {
 				continue;
 			}
@@ -95,13 +33,6 @@ const fillMarkers = (grid, markers, max = 4) => {
 				if (rsymbol > 0) marker[rsymbol - 1] = false;
 				if (csymbol > 0) marker[csymbol - 1] = false;
 				if (bsymbol > 0) marker[bsymbol - 1] = false;
-			}
-			let count = 0;
-			for (let i = 0; i < 9; i++) {
-				if (marker[i]) count++;
-			}
-			if (count > max) {
-				delete markers[index];
 			}
 		}
 	}
@@ -127,32 +58,137 @@ const fillSingles = (grid, markers) => {
 			if (symbol > 0) {
 				delete markers[i];
 				grid[i] = symbol;
+				return true;
 			}
 		}
 	}
-	fillSingles2(grid, markers);
+	return false;
 }
 
-const fillSingles2 = (grid, markers) => {
-	for (let r = 0; r < 9; r++) {
-		for (let i = 0; i < 9; i++) {
-			let col = -1;
+const fillMissingSingles = (grid, markers) => {
+	for (let i = 0; i < 9; i++) {
+		for (let r = 0; r < 9; r++) {
+			let indexCol = -1;
 			for (let c = 0; c < 9; c++) {
-				const marker = markers[r * 9 + c];
-				if (r === 0 && i === 0) console.log(marker[i]);
-				if (marker && marker[i]) {
-					if (col === -1) col = c;
+				const markerCol = markers[r * 9 + c];
+				if (markerCol && markerCol[i]) {
+					if (indexCol === -1) indexCol = c;
 					else {
-						col = -1;
+						indexCol = -1;
 						break;
 					}
 				}
 			}
-			if (col !== -1) {
-				grid.setSymbol(r, col, i + 1);
+			if (indexCol !== -1) {
+				grid[r * 9 + indexCol] = i + 1;
+				delete markers[r * 9 + indexCol];
+				return true;
 			}
+		}
+
+		for (let c = 0; c < 9; c++) {
+			let indexRow = -1;
+			for (let r = 0; r < 9; r++) {
+				const markerRow = markers[r * 9 + c];
+				if (markerRow && markerRow[i]) {
+					if (indexRow === -1) indexRow = r;
+					else {
+						indexRow = -1;
+						break;
+					}
+				}
+			}
+			if (indexRow !== -1) {
+				grid[indexRow * 9 + c] = i + 1;
+				delete markers[indexRow * 9 + c];
+				return true;
+			}
+		}
+
+		for (let j = 0; j < 9; j++) {
+			const brow = Math.floor(j / 3) * 3;
+			const bcol = (j % 3) * 3;
+			let indexBox = -1;
+			for (let b = 0; b < 9; b++) {
+				const r = brow + Math.floor(b / 3);
+				const c = bcol + b % 3;
+				const markerBox = markers[r * 9 + c];
+				if (markerBox && markerBox[i]) {
+					if (indexBox === -1) indexBox = r;
+					else {
+						indexBox = -1;
+						break;
+					}
+				}
+			}
+			if (indexBox !== -1) {
+				const r = brow + Math.floor(indexBox / 3);
+				const c = bcol + indexBox % 3;
+
+				grid[r * 9 + c] = i + 1;
+				delete markers[r * 9 + c];
+				return true;
+			}
+		}
+
+		return false;
+	}
+	for (let y = 0; y < 9; y++) {
+		for (let i = 0; i < 9; i++) {
+			// let indexCol = -1;
+			// for (let x = 0; x < 9; x++) {
+			// 	const markerRow = markers[y * 9 + x];
+			// 	if (markerRow && markerRow[i]) {
+			// 		if (indexCol === -1) indexCol = x;
+			// 		else {
+			// 			indexCol = -1;
+			// 			break;
+			// 		}
+			// 	}
+			// }
+			// if (indexCol !== -1) {
+			// 	grid.setSymbol(y, indexCol, i + 1);
+			// 	delete markers[y * 9 + indexCol];
+			// }
+
+			// let indexRow = -1;
+			// for (let x = 0; x < 9; x++) {
+			// 	const markerCol = markers[x * 9 + y];
+			// 	if (markerCol && markerCol[i]) {
+			// 		if (indexRow === -1) indexRow = y;
+			// 		else {
+			// 			indexRow = -1;
+			// 			break;
+			// 		}
+			// 	}
+			// }
+			// if (indexRow !== -1) {
+			// 	grid.setSymbol(indexRow, y, i + 1);
+			// 	delete markers[indexRow * 9 + y];
+			// }
+
+			// let indexBox = -1;
+			// for (let x = 0; x < 9; x++) {
+			// 	const xbase = Math.floor(x / 3) * 3 + (y % 3) * 3;
+			// 	const ybase = Math.floor(y % 3) * 3 + Math.floor(x / 3);
+			// 	const markerRow = markers[ybase * 9 + xbase];
+			// 	if (markerRow && markerRow[i]) {
+			// 		if (indexBox === -1) indexBox = ybase * 9 + xbase;
+			// 		else {
+			// 			indexBox = -1;
+			// 			break;
+			// 		}
+			// 	}
+			// }
+			// if (indexBox !== -1) {
+			// 	const xbase = Math.floor(y / 3) * 3 + indexBox % 3;
+			// 	const ybase = Math.floor(y % 3) * 3 + Math.floor(indexBox / 3);
+
+			// 	// grid[indexBox] = i + 1;
+			// 	// delete markers[indexBox];
+			// }
 		}
 	}
 }
 
-export { fillMarkers, fillSingles };
+export { fillMarkers, fillSingles, fillMissingSingles };
