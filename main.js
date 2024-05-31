ca675b1647d871eebe55feb4f4bc2fb4c00afe0d
ScottNovakowski <snovakow@gmail.com>
2024-05-31 14:51:50 -0400

    0.2.1

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const appInitialize = (title, version, css) => {
 
 	console.log(title + " Version: " + version);
 }
-appInitialize("Sudoku", "0.2.0");
+appInitialize("Sudoku", "0.2.1");
 
 document.body.style.color = 'black';
 document.body.style.backgroundColor = 'white';
