3f9554eb2f4565b1d55e2b74c6ba8d70824ac119
ScottNovakowski <snovakow@gmail.com>
2024-06-02 19:29:34 -0400

    Update solver.js

--- solver.js
+++ solver.js
@@ -1,4 +1,4 @@
-const fillMarkers = (grid, markers) => {
+const candidates = (grid, markers) => {
 	for (let i = 0; i < 81; i++) {
 		if (grid[i] === 0) {
 			const marker = markers[i];
@@ -38,7 +38,7 @@ const fillMarkers = (grid, markers) => {
 	}
 }
 
-const fillSingles = (grid, markers) => {
+const missingCells = (grid, markers) => {
 	for (let i = 0; i < 81; i++) {
 		if (grid[i] === 0) {
 			let symbol = -1;
@@ -63,6 +63,16 @@ const fillSingles = (grid, markers) => {
 		}
 	}
 	return false;
+}
+
+const rowForLoc = (x) => {
+	return Math.floor(x / 9);
+}
+const colForLoc = (x) => {
+	return x % 9;
+}
+const boxLoc = (x) => {
+	return Math.floor(x / 27) * 3 + Math.floor((x % 9) / 3);
 }
 
 class GridGroup {
@@ -100,7 +110,7 @@ class GridGroup {
 	}
 }
 
-const fillMissingSingles = (grid, markers) => {
+const nakedCells = (grid, markers) => {
 	const markerGroup = new GridGroup(markers);
 
 	class Type {
@@ -147,7 +157,7 @@ class SetUnit {
 	}
 }
 
-const fillGroups = (markers) => {
+const hiddenCells = (markers) => { // single double any
 	const markerGroup = new GridGroup(markers);
 	const union = new Set();
 
@@ -219,72 +229,166 @@ const fillGroups = (markers) => {
 
 const pairGroups = (markers) => {
 	const markerGroup = new GridGroup(markers);
-	const union = new Set();
 
-	const groupTypes = ['getRow', 'getCol', 'getBox'];
-	for (const getGroup of groupTypes) {
+	for (let i = 0; i < 9; i++) {
+
+		// Box - Remove Col Row
 		for (let x = 0; x < 9; x++) {
-			const sets = [];
+			let y1 = -1;
+			let y2 = -1;
+
 			for (let y = 0; y < 9; y++) {
-				const marker = markerGroup[getGroup](x, y);
+				const marker = markerGroup.getBox(x, y);
 				if (!marker) continue;
 
-				const set = new Set();
-				for (let i = 0; i < 9; i++) {
-					const symbol = marker[i];
-					if (symbol) set.add(i);
+				const symbol = marker[i];
+				if (!symbol) continue;
+
+				if (y1 === -1) y1 = y;
+				else if (y2 === -1) y2 = y;
+				else {
+					y2 = -1;
+					break;
 				}
-				if (set.size > 0) sets.push(new SetUnit(y, set));
 			}
-			const len = sets.length;
-			for (let i = 0; i < len - 1; i++) {
-				const remainder = len - i - 1;
-				const setUnit = sets[i];
 
-				const endLen = 0x1 << remainder;
-				for (let inc = 1; inc < endLen; inc++) {
-					union.clear();
-					for (const x of setUnit.set) union.add(x);
-					let unionCount = 1;
-					let setHitMask = 0x1 << i;
+			if (y2 >= 0) {
+				const loc1 = GridGroup.boxLoc(x, y1);
+				const loc2 = GridGroup.boxLoc(x, y2);
 
-					let mask = 0x1;
-					for (let j = i + 1; j < len; j++) {
-						const state = inc & mask;
-						if (state > 0) {
-							const compare = sets[j];
-							for (const x of compare.set) union.add(x);
-							unionCount++;
-							setHitMask |= 0x1 << j;
-						}
-						mask <<= 1;
-					}
-
+				const colLoc1 = colForLoc(loc1);
+				const colLoc2 = colForLoc(loc2);
+				if (colLoc1 === colLoc2) {
 					let reduced = false;
-					if (unionCount === union.size && unionCount < sets.length) {
-						for (let shift = 0; shift < len; shift++) {
-							if ((setHitMask & (0x1 << shift)) > 0) continue;
 
-							const set = sets[shift];
-							const marker = markerGroup[getGroup](x, set.index);
-							if (!marker) continue;
-
-							for (const symbol of union) {
-								if (marker[symbol]) {
-									marker[symbol] = false;
-									reduced = true;
-								}
-							}
+					for (let j = 0; j < 9; j++) {
+						if (Math.floor(x / 3) === Math.floor(j / 3)) continue;
+						const outer = markerGroup.getCol(colLoc1, j);
+						if (!outer) continue;
+						const symbol = outer[i];
+						if (symbol) {
+							outer[i] = false;
+							reduced = true;
 						}
-						// console.log("Found Group " + getGroup);
+					}
+					if (reduced) return true;
+				}
+
+				const rowLoc1 = rowForLoc(loc1);
+				const rowLoc2 = rowForLoc(loc2);
+				if (rowLoc1 === rowLoc2) {
+					let reduced = false;
+
+					for (let j = 0; j < 9; j++) {
+						if ((x % 3) === Math.floor(j / 3)) continue;
+						const outer = markerGroup.getRow(rowLoc1, j);
+						if (!outer) continue;
+						const symbol = outer[i];
+						if (symbol) {
+							outer[i] = false;
+							reduced = true;
+						}
 					}
 					if (reduced) return true;
 				}
 			}
+
+		}
+
+		// Row - Remove Box
+		for (let x = 0; x < 9; x++) {
+			let y1 = -1;
+			let y2 = -1;
+
+			for (let y = 0; y < 9; y++) {
+				const marker = markerGroup.getRow(x, y);
+				if (!marker) continue;
+
+				const symbol = marker[i];
+				if (!symbol) continue;
+
+				if (y1 === -1) y1 = y;
+				else if (y2 === -1) y2 = y;
+				else {
+					y2 = -1;
+					break;
+				}
+			}
+
+			if (y2 >= 0) {
+				const loc1 = GridGroup.rowLoc(x, y1);
+				const loc2 = GridGroup.rowLoc(x, y2);
+
+				const boxLoc1 = boxLoc(loc1);
+				const boxLoc2 = boxLoc(loc2);
+				if (boxLoc1 === boxLoc2) {
+					let reduced = false;
+
+					for (let j = 0; j < 9; j++) {
+						if ((boxLoc1 % 3) === Math.floor(j / 3)) continue;
+						const outer = markerGroup.getRow(x, j);
+						if (!outer) continue;
+						const symbol = outer[i];
+						if (symbol) {
+							outer[i] = false;
+							reduced = true;
+							console.log("Row");
+						}
+					}
+					if (reduced) return true;
+				}
+			}
+
+		}
+
+		// Col - Remove Box
+		for (let x = 0; x < 9; x++) {
+			let y1 = -1;
+			let y2 = -1;
+
+			for (let y = 0; y < 9; y++) {
+				const marker = markerGroup.getCol(x, y);
+				if (!marker) continue;
+
+				const symbol = marker[i];
+				if (!symbol) continue;
+
+				if (y1 === -1) y1 = y;
+				else if (y2 === -1) y2 = y;
+				else {
+					y2 = -1;
+					break;
+				}
+			}
+
+			if (y2 >= 0) {
+				const loc1 = GridGroup.colLoc(x, y1);
+				const loc2 = GridGroup.colLoc(x, y2);
+
+				const boxLoc1 = boxLoc(loc1);
+				const boxLoc2 = boxLoc(loc2);
+				if (boxLoc1 === boxLoc2) {
+					let reduced = false;
+
+					for (let j = 0; j < 9; j++) {
+						if (Math.floor(boxLoc1 / 3) === Math.floor(j / 3)) continue;
+						const outer = markerGroup.getCol(x, j);
+						if (!outer) continue;
+						const symbol = outer[i];
+						if (symbol) {
+							outer[i] = false;
+							reduced = true;
+							console.log("Col");
+						}
+					}
+					if (reduced) return true;
+				}
+			}
+
 		}
 	}
 
 	return false;
 }
 
-export { fillMarkers, fillSingles, fillMissingSingles, fillGroups, pairGroups };
+export { candidates, missingCells, nakedCells, hiddenCells, pairGroups };
