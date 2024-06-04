3cf10110d57bf39d78d45b569cd846f1fd9ea3f4
ScottNovakowski <snovakow@gmail.com>
2024-06-04 19:49:25 -0400

    Update solver.js

--- solver.js
+++ solver.js
@@ -178,16 +178,32 @@ class GridGroup {
 	}
 }
 
+const TypeRow = "ROW";
+const TypeCol = "COL";
+const TypeBox = "BOX";
 class Type {
-	constructor(group, index) {
-		this.group = group;
-		this.index = index;
+	constructor(type) {
+		switch (type) {
+			case TypeRow:
+				this.group = 'getRow';
+				this.index = 'rowIndex';
+				break;
+			case TypeCol:
+				this.group = 'getCol';
+				this.index = 'colIndex';
+				break;
+			case TypeBox:
+				this.group = 'getBox';
+				this.index = 'boxIndex';
+				break;
+		}
+		this.type = type;
 	}
 }
+const groupTypes = [new Type(TypeRow), new Type(TypeCol), new Type(TypeBox)];
 
 const nakedCells = (grid, markers) => {
 	const markerGroup = new GridGroup(markers);
-	const groupTypes = [new Type('getRow', 'rowIndex'), new Type('getCol', 'colIndex'), new Type('getBox', 'boxIndex')];
 
 	for (const groupType of groupTypes) {
 		const getGroup = groupType.group;
@@ -229,7 +245,6 @@ const hiddenCells = (markers) => { // single double any
 	const markerGroup = new GridGroup(markers);
 	const union = new Set();
 
-	const groupTypes = ['getRow', 'getCol', 'getBox'];
 	for (const getGroup of groupTypes) {
 		for (let x = 0; x < 9; x++) {
 			const sets = [];
@@ -362,8 +377,6 @@ const pairGroups = (markers) => {
 			}
 
 		}
-
-		const groupTypes = [new Type('getRow', 'rowIndex'), new Type('getCol', 'colIndex')];
 
 		for (const groupType of groupTypes) {
 			const getGroup = groupType.group;
