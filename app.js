c76eef0df204d659edf30ba46173f3d388408417
ScottNovakowski <snovakow@gmail.com>
2024-08-24 16:21:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,50 +2,6 @@ import { FONT, board } from "../sudokulib/board.js";
 import { consoleOut, fillSolve } from "../sudokulib/generator.js";
 import { picker, pickerDraw, pickerMarker, pixAlign } from "../sudokulib/picker.js";
 
-const raws = [
-	"19 clue 1 simple",
-	'003400000700000000000010600060000000000070040050002930000003000000065800914000000'.split(''),
-	"19 clue 2 brute",
-	'000050700000100000590003600300000007000060400800070000006090000000000008009000031'.split(''),
-	"19 clue 3 simple",
-	'000000080060200010050007040070065000800001090000000002009004000400000000002000005'.split(''),
-	"19 clue 4 simple",
-	'020056000008000000090070300480010000000000020000000050300009000700002800000000407'.split(''),
-	"30 clue 1",
-	'000400080008029030090003004370060048900000600680002375000200007007005890000017000'.split(''),
-	"30 clue 2",
-	'023400000500300000600000400302000608000010004805030921001200006006100300438960000'.split(''),
-	"30 clue 3",
-	'000050080000000204748000601090000170800700940600900008500200093280004000007090810'.split(''),
-	"30 clue 4",
-	'000000780067280140000900300046700000210500000030060004000000407001009050654007901'.split(''),
-	"30 clue 5",
-	'000400700068903010004080006046109002000000000800000050680210500010035208200004600'.split(''),
-	"30 clue 6",
-	'023400709000900314090010000201000500000000000060040032600090871009070050002130040'.split(''),
-	"30 clue 7",
-	'120450080004009035089030060400300507302547900000000003001000000000800090000904050'.split(''),
-	"30 clue 8",
-	'023006080540080001807001000000020000301070200002010097734000102000000005605000038'.split(''),
-	"30 clue 9",
-	'000000000589107000070002005000003800800571002030208010950000071000000290010720043'.split(''),
-];
-const sudokuSamples = [];
-for (let rawIndex = 0; rawIndex < raws.length; rawIndex += 2) {
-	const name = raws[rawIndex];
-	const raw = raws[rawIndex + 1];
-	const puzzle = [];
-	for (let i = 0, index = 0; i < 9; i++) {
-		const row = [];
-		for (let j = 0; j < 9; j++, index++) {
-			row[j] = raw[index];
-		}
-		puzzle[i] = row;
-	}
-	puzzle[9] = name;
-	sudokuSamples.push(puzzle);
-}
-
 let selectedRow = 0;
 let selectedCol = 0;
 let selected = false;
@@ -62,52 +18,6 @@ const draw = () => {
 	pickerDraw(font);
 }
 
-const createSelect = (options, onChange) => {
-	const select = document.createElement('select');
-
-	for (const title of options) {
-		const option = document.createElement('option');
-		option.text = title;
-		select.appendChild(option);
-	}
-
-	select.addEventListener('change', () => {
-		onChange(select);
-	});
-	document.body.appendChild(select);
-
-	document.body.appendChild(document.createElement('br'));
-
-	return select;
-};
-
-const names = [];
-for (const sudoku of sudokuSamples) names.push(sudoku[9]);
-
-const selector = createSelect(["-", ...names], (select) => {
-	if (select.selectedIndex === 0) {
-		for (let i = 0; i < 81; i++) {
-			const cell = board.cells[i];
-			cell.show = false;
-			cell.setSymbol(0);
-			board.startCells[i].symbol = 0;
-		}
-		localStorage.removeItem("gridName");
-		saveGrid();
-		draw();
-		return;
-	}
-
-	selected = false;
-
-	const index = select.selectedIndex - 1;
-	board.setGrid(index < sudokuSamples.length ? sudokuSamples[index] : sudokuSamples[index - sudokuSamples.length]);
-	saveGrid(select.selectedIndex);
-	draw();
-});
-selector.style.position = 'absolute';
-selector.style.width = '40px';
-
 const DataVersion = "0.2";
 
 const saveGrid = (selectedIndex = null) => {
@@ -118,12 +28,6 @@ const saveGrid = (selectedIndex = null) => {
 };
 const loadGrid = () => {
 	if (localStorage.getItem("DataVersion") !== DataVersion) return false;
-
-	const selectedIndex = localStorage.getItem("gridName");
-	if (selectedIndex !== null) {
-		const selectedInt = parseInt(selectedIndex);
-		if (selectedInt > 0 && selectedInt < selector.options.length) selector.selectedIndex = selectedInt;
-	}
 
 	const startGrid = localStorage.getItem("startGrid");
 	if (!startGrid) return false;
@@ -198,8 +102,6 @@ const pickerClick = (event) => {
 	}
 
 	draw();
-
-	saveGrid(selector.selectedIndex);
 };
 picker.addEventListener('click', pickerClick);
 
@@ -223,8 +125,6 @@ const pickerMarkerClick = (event) => {
 	}
 
 	draw();
-
-	saveGrid(selector.selectedIndex);
 };
 pickerMarker.addEventListener('click', pickerMarkerClick);
 
@@ -279,7 +179,6 @@ markerButton.addEventListener('click', () => {
 });
 document.body.appendChild(markerButton);
 
-selector.style.transform = 'translateX(-50%)';
 clearButton.style.transform = 'translateX(-50%)';
 markerButton.style.transform = 'translateX(-50%)';
 
@@ -304,9 +203,6 @@ const resize = () => {
 		markerButton.style.bottom = '324px';
 		markerButton.style.left = '96px';
 
-		selector.style.bottom = '288px';
-		selector.style.left = '96px';
-
 		clearButton.style.bottom = '200px';
 		clearButton.style.left = '96px';
 	} else {
@@ -325,9 +221,6 @@ const resize = () => {
 		markerButton.style.bottom = '128px';
 		markerButton.style.left = '50%';
 
-		selector.style.bottom = '96px';
-		selector.style.left = '50%';
-
 		clearButton.style.bottom = '8px';
 		clearButton.style.left = '50%';
 	}
@@ -339,21 +232,38 @@ const resize = () => {
 	board.canvas.height = Math.floor(size * window.devicePixelRatio / 1) * 2;
 
 	draw();
+};
+resize();
 
+window.addEventListener('resize', resize);
+
+if (window.location.hash.length === 82) {
+	console.log(window.location.hash.substring(1));
+	const hash = window.location.hash.substring(1);
+	board.cells.fromString(hash);
+	for (const cell of board.cells) {
+		cell.show = false;
+		const startCell = board.startCells[cell.index];
+		startCell.symbol = cell.symbol;
+	}
+	draw();
+} else {
 	const xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = () => {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			board.cells.fromString(xhttp.responseText);
-			for (let i = 0; i < 81; i++) {
-				const cell = board.startCells[i];
-				cell.symbol = board.cells[i].symbol;
+			for (const cell of board.cells) {
+				cell.show = false;
+				const startCell = board.startCells[cell.index];
+				startCell.symbol = cell.symbol;
 			}
+
+			window.location.hash = "#" + xhttp.responseText;
+			// window.history.pushState({}, '', '/new-url');
+
 			draw();
 		}
 	};
 	xhttp.open("GET", "../sudokulib/sudoku.php" + window.location.search, true);
 	xhttp.send();
-};
-resize();
-
-window.addEventListener('resize', resize);
+}
