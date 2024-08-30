121b31f38ef6bff649eaab0375ee44f2896fd3a3
ScottNovakowski <snovakow@gmail.com>
2024-08-30 09:50:10 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
 	mode: "production",
 	entry: './main.js',
 	output: {
-		filename: '[chunkhash].js',
+		filename: 'main.js',
 		path: path.resolve(__dirname, '../../live/sudoku'),
 		chunkFilename: '[chunkhash].js',
 		clean: true,
 		publicPath: ''
 	},
 	plugins: [
-		new HtmlWebpackPlugin({
-			title: ""
+		new CopyPlugin({
+			patterns: [
+				{ from: './index.html', to: 'index.html' }
+			]
 		})
-	],
+	]
 };
