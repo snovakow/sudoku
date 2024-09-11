6523bec2557a92639eb7980eba4e6bbbb1f6f46c
ScottNovakowski <snovakow@gmail.com>
2024-09-11 19:23:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const src = document.currentScript.src;
 import("../snovakow/lib/main.js").then(
 	main => {
-		const gtag = (window.location.host === "snovakow.com") ? 'G-DV1KLMY93N' : false;
-		const options = { main: src, gtag };
-		main.initialize(options).then(() => import("./app.js"));
+		const urlComicSans = 'url(../snovakow/assets/fonts/comic-sans-ms/design.graffiti.comicsansms.ttf)';
+		const urlOpenSansRegular = 'url(../snovakow/assets/fonts/Open_Sans/static/OpenSans-Regular.ttf)';
+		const urlOpenSansLight = 'url(../snovakow/assets/fonts/Open_Sans/static/OpenSans-Light.ttf)';
+
+		const fontOpenSansLight = new FontFace("LIGHT", urlOpenSansLight);
+		const fontOpenSansRegular = new FontFace("REGULAR", urlOpenSansRegular);
+		const fontComicSans = new FontFace("COMIC", urlComicSans);
+
+		document.fonts.add(fontOpenSansLight);
+		document.fonts.add(fontOpenSansRegular);
+		document.fonts.add(fontComicSans);
+
+		fontOpenSansLight.load();
+		fontOpenSansRegular.load();
+		fontComicSans.load();
+
+		document.fonts.ready.then(() => {
+			const gtag = (window.location.host === "snovakow.com") ? 'G-DV1KLMY93N' : false;
+			const options = { main: src, gtag };
+			main.initialize(options).then(() => import("./app.js"));
+		});
 	}
 );
