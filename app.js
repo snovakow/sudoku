e69cec313e61f42ec8ee966ecb89d1ffa3e5140c
ScottNovakowski <snovakow@gmail.com>
2024-10-10 10:13:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -744,7 +744,7 @@ if (strategy === 'custom') {
 
 			const solve = () => {
 				if (superpositionMode === 1) fillSolve(board.cells, STRATEGY.NONE);
-				else fillSolve(board.cells, STRATEGY.ALL);
+				else fillSolve(board.cells, STRATEGY.ALL_STRICT);
 			}
 
 			// Candidates
